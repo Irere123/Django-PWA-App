d70db67b200e2deaf1f4764d6e307deb589c7bed
irere emmanuel <irere2050@gmail.com>
2020-10-02 22:21:51 +1400

    urls

--- documentation/urls.py
+++ documentation/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 from . import views
 
-app_name = 'documantion'
+app_name = 'documentation'
 urlpatterns = [
+    
     path('resources/', views.index, name='index')
 ]
