c4b4bba1642709d8a6053829bf1dafc5f57ed06c
irere emmanuel <irere2050@gmail.com>
2020-10-04 07:07:40 +1400

    add a results page for searches

--- documentation/urls.py
+++ documentation/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 app_name = 'documentation'
 urlpatterns = [
-    
+    # Page for Search Results
+    path('results/', views.SearchResultsListView.as_view(), name='results'),
     path('resources/', views.index, name='index')
 ]
--- documentation/views.py
+++ documentation/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+from django.views.generic import ListView
+from django.db.models import Q
 from .models import Document
 
 # Create your views here.
@@ -6,3 +8,14 @@ def index(request):
     documents = Document.objects.all()
     context = {'documents': documents}
     return render(request, 'documentation/resources.html', context)
+
+class SearchResultsListView(ListView):
+    model = Document
+    context_object_name = 'documents_list'
+    template_name = 'documentation/results.html'
+    
+    def get_queryset(self):
+        query = self.request.GET.get('q')
+        return Document.objects.filter(
+            Q(title__icontains =query) | Q(content__icontains = query) | Q(cover_image__icontains=query)
+        )
