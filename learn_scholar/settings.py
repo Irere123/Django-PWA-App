36e143cea3a1ba041e1dd2759deebf7bbc6bad06
irere emmanuel <irere2050@gmail.com>
2020-10-08 13:13:29 +1400

    changes

--- learn_scholar/settings.py
+++ learn_scholar/settings.py
@@ -194,7 +194,7 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'irere2050@gmail.com'
+EMAIL_HOST_USER = 'learnscholar.com@gmail.com'
 EMAIL_HOST_PASSWORD = 'irere2045 emmy'
 
 
